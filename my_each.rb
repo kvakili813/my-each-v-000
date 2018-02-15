4ee6d2f42d74fc7ed23f1e3af9008d54c0a0e58b
Kayvan Vakili <kvakili813@gmail.com>
2018-02-15 02:53:25 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(words)# put argument(s) here
+i = 0
+  #my_each(words) do |i|
+while i < words.length
+  yield words[i]
+  i = i + 1
+end
+    words
 end
